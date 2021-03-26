fd6ec8c99eb98cd9b1c72a06d7f1ec8119d1c534
Zaraksh Rahman <zarakshR@pm.me>
2021-03-26 16:08:56 +0530

    Added support for changing browser and bookmarks file. Fixed program opening each bookmark when quit unexpectedly

--- rofiBookmarks.sh
+++ rofiBookmarks.sh
@@ -1,3 +1,12 @@
-name=$(cat exampleBookmarks | cut -d '|' -f 1 | rofi -dmenu)
-url=$(cat exampleBookmarks | grep "$name" | cut -d '|' -f 2)
-firefox-beta $url
+# TODO: 
+
+BOOKMARKS="$HOME/Code/rofiBookmarks/exampleBookmarks"
+BROWSER="firefox-beta"
+
+# This calls a rofi -dmenu instance with the names of the bookmarks, the output (i.e., $name) is the selected entry
+name=$(cat $BOOKMARKS | cut -d '|' -f 1 | rofi -dmenu -p Deskman)
+
+if [[ -n $name ]]; then
+    url=$(cat $BOOKMARKS | grep "$name" | cut -d '|' -f 2)
+    $BROWSER $url
+fi

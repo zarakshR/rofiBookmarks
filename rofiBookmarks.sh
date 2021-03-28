1731538abf7e024ba7e0776bd2a0399ba845155e
Zaraksh Rahman <zarakshR@pm.me>
2021-03-29 03:59:46 +0530

    No. of entries in rofi now dynamically allocated based on no. of entries in bookmarks file.

--- rofiBookmarks.sh
+++ rofiBookmarks.sh
@@ -1,8 +1,8 @@
 BOOKMARKS="$HOME/Code/rofiBookmarks/exampleBookmarks"
 BROWSER="firefox-beta"
 
-# This calls a rofi -dmenu instance with the names of the bookmarks, the output (i.e., $name) is the selected entry
-name=$(cat $BOOKMARKS | cut -d '|' -f 1 | rofi -dmenu -p Deskman)
+num=$(cat $BOOKMARKS | wc -l)
+name=$(cat $BOOKMARKS | cut -d '|' -f 1 | rofi -dmenu -l $num -p Bookmarks)
 
 if [[ -n $name ]]; then
     url=$(cat $BOOKMARKS | grep "$name" | cut -d '|' -f 2)
